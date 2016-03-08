cf9166792fb6fae1f0591eadfedaf23fe6d698d4
Gary Coulbourne <gcoulbourne@leadid.com>
2016-03-08 14:33:34 -0500

    react to interaction

--- src/main/scala/Mandelbrot/Mandelbrot.scala
+++ src/main/scala/Mandelbrot/Mandelbrot.scala
@@ -5,6 +5,7 @@ import java.awt.image.BufferedImage
 
 import scala.annotation.tailrec
 import scala.swing._
+import scala.swing.event._
 
 object Mandelbrot extends SimpleSwingApplication {
 
@@ -12,26 +13,53 @@ object Mandelbrot extends SimpleSwingApplication {
   private val topMand:Double    = -1
   private val rightMand:Double  = 2
   private val bottomMand:Double = 1
+
   private val pixelWidth    = 800
   private val pixelHeight   = 400
 
-  private val width:Double  = Math.abs(leftMand - rightMand)
-  private val height:Double = Math.abs(topMand - bottomMand)
-  private val wpixelDelta:Double = width/pixelWidth
-  private val hpixelDelta:Double = height/pixelHeight
+  private var width:Double  = Math.abs(leftMand - rightMand)
+  private var height:Double = Math.abs(topMand - bottomMand)
+  private var wpixelDelta:Double = width/pixelWidth
+  private var hpixelDelta:Double = height/pixelHeight
 
   private val topOut = 10000
 
   private val img:BufferedImage = new BufferedImage(pixelWidth, pixelHeight, BufferedImage.TYPE_3BYTE_BGR)
 
-  private val lock = new Object
-
   val grad = new GradientMaker(topOut, RGB(255,0,0), RGB(0,0,255))
 
-  lazy val ui = new Panel {
+  var xclick:Double = 0
+  var yclick:Double = 0
+
+  lazy val ui = new BorderPanel {
     background = Color.white
     preferredSize = new Dimension(pixelWidth, pixelHeight)
     focusable = true
+
+    listenTo(mouse.clicks, mouse.moves)
+
+    reactions += {
+      case e: UIElementResized => println("Panel RESIZED")
+
+      case e: MousePressed => {
+        xclick = e.point.getX
+        yclick = e.point.getY
+        println(s"Mouse Pressed $xclick, $yclick")
+      }
+      case e: MouseDragged => println("Mouse Dragged")
+      case e: MouseReleased => println("Mouse Released")
+
+      /* case e: MousePressed =>
+        moveTo(e.point)
+        requestFocusInWindow()
+      case e: MouseDragged => lineTo(e.point)
+      case e: MouseReleased => lineTo(e.point)
+      case KeyTyped(_, 'c', _, _) =>
+        path = new geom.GeneralPath
+        repaint()*/
+
+      case _: FocusLost => repaint()
+    }
 
     var drawThread = new Thread(new Runnable {
       def run = {
@@ -79,6 +107,10 @@ object Mandelbrot extends SimpleSwingApplication {
   def top = new MainFrame {
     title = "Mandelbrot Drawing"
     contents = ui
+
+    reactions += {
+      case e: UIElementResized => println("Main Frame RESIZED")
+    }
   }
 
 
